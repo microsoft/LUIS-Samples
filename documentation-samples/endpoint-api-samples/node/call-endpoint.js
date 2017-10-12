6d39e500d52cea96c11332d8c8adf3fac9dd1687
Denise Mak <v-demak@microsoft.com>
2017-10-12 16:21:39 -0700

    Fix typo in comment

--- documentation-samples/endpoint-api-samples/node/call-endpoint.js
+++ documentation-samples/endpoint-api-samples/node/call-endpoint.js
@@ -7,7 +7,7 @@ function getLuisIntent(utterance) {
     var endpoint =
         "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
 
-    // Set the LUIS_SUBSCRIPTION_KEY environment variable 
+    // Set the LUIS_APP_ID environment variable 
     // to 60340f5f-99c1-4043-8ab9-c810ff16252d, which is the ID
     // of a public sample application.    
     var luisAppId = process.env.LUIS_APP_ID;
