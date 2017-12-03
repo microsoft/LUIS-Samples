a81ed2e0e213b7d6e9eb100f6751f116b9b42a6e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-12-03 17:48:35 +0100

    Update NodeJS sample to use ES6 syntax
    The minimum version required by deployment script in repository is version 6 of
    NodeJS. Also, the online docs samples seem to already be updated to use modern syntax
    as well, for example:
    https://docs.microsoft.com/en-us/azure/cognitive-services/bing-spell-check/quickstarts/nodejs
    This commit updates:
    - native Promises support without external library dependency
    - variable assignments
    - functions declarations to fat arrows
    - string templates
    - Array operations are rewritten to use map/filter/fill
    
    Thanks!

--- bot-integration-samples/hotel-finder/nodejs/store.js
+++ bot-integration-samples/hotel-finder/nodejs/store.js
@@ -1,6 +1,4 @@
-var Promise = require('bluebird');
-
-var ReviewsOptions = [
+const ReviewsOptions = [
     '“Very stylish, great stay, great staff”',
     '“good hotel awful meals”',
     '“Need more attention to little things”',
@@ -8,45 +6,46 @@ var ReviewsOptions = [
     '“Positive surprise”',
     '“Beautiful suite and resort”'];
 
+const searchHotels = (destination) => {
+    return new Promise((resolve, reject) => {
+        // Filling the hotels results manually just for demo purposes
+        let hotels = Array(5).fill({});
+        hotels = hotels.map((hotel, index) => {
+            const i = index + 1;
+            return {
+                name: `${destination} Hotel ${i}`,
+                location: destination,
+                rating: Math.ceil(Math.random() * 5),
+                numberOfReviews: Math.floor(Math.random() * 5000) + 1,
+                priceStarting: Math.floor(Math.random() * 450) + 80,
+                image: `https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel+${i}&w=500&h=260`
+            };
+        });
+        hotels.sort((a, b) => { return a.priceStarting - b.priceStarting; });
+        // complete promise with a timer to simulate async response
+        setTimeout(() => { resolve(hotels); }, 1000);
+    });
+}
+
+const searchHotelReviews = (hotelName) => {
+    return new Promise((resolve, reject) => {
+        // Filling the review results manually just for demo purposes
+        let reviews = Array(5).fill({});
+        reviews = reviews.map(review => {
+            return {
+                title: ReviewsOptions[Math.floor(Math.random() * ReviewsOptions.length)],
+                text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris odio magna, sodales vel ligula sit amet, vulputate vehicula velit.
+                Nulla quis consectetur neque, sed commodo metus.`,
+                image: 'https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif'
+            };
+        });
+        // complete promise with a timer to simulate async response
+        setTimeout(() => { resolve(reviews); }, 1000);
+    });
+}
+
 module.exports = {
-    searchHotels: function (destination) {
-        return new Promise(function (resolve) {
-
-            // Filling the hotels results manually just for demo purposes
-            var hotels = [];
-            for (var i = 1; i <= 5; i++) {
-                hotels.push({
-                    name: destination + ' Hotel ' + i,
-                    location: destination,
-                    rating: Math.ceil(Math.random() * 5),
-                    numberOfReviews: Math.floor(Math.random() * 5000) + 1,
-                    priceStarting: Math.floor(Math.random() * 450) + 80,
-                    image: 'https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel+' + i + '&w=500&h=260'
-                });
-            }
-
-            hotels.sort(function (a, b) { return a.priceStarting - b.priceStarting; });
-
-            // complete promise with a timer to simulate async response
-            setTimeout(function () { resolve(hotels); }, 1000);
-        });
-    },
-
-    searchHotelReviews: function (hotelName) {
-        return new Promise(function (resolve) {
-
-            // Filling the review results manually just for demo purposes
-            var reviews = [];
-            for (var i = 0; i < 5; i++) {
-                reviews.push({
-                    title: ReviewsOptions[Math.floor(Math.random() * ReviewsOptions.length)],
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris odio magna, sodales vel ligula sit amet, vulputate vehicula velit. Nulla quis consectetur neque, sed commodo metus.',
-                    image: 'https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif'
-                });
-            }
-
-            // complete promise with a timer to simulate async response
-            setTimeout(function () { resolve(reviews); }, 1000);
-        });
-    }
+    searchHotels,
+    searchHotelReviews
 };
