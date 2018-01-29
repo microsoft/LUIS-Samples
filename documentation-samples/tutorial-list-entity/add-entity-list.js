6d176689c96b4fee9e4b74abf214c2a085bcb3d9
v-geberr <v-geberr@microsoft.com>
2018-01-29 11:21:55 -0800

    1st

---
+++ documentation-samples/tutorial-list-entity/add-entity-list.js
@@ -0,0 +1,68 @@
+/*-----------------------------------------------------------------------------
+This template demonstrates how to use list entities
+. 
+For a complete walkthrough of creating this type of bot see the article at
+https://aka.ms/
+-----------------------------------------------------------------------------*/
+
+// NPM Dependencies
+var request = require('request-promise');
+
+// To run this sample, change these constants.
+
+// Programmatic/starter key, available in www.luis.ai under Account Settings
+const programmaticKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+// ID of your LUIS app to which you want to add an utterance
+const appId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+
+// Version number of your LUIS app
+const versionId = "0.1";
+
+// Region of app
+const region = "westus";
+
+// Construct HTTP uri
+const uri= `https://${region}.api.cognitive.microsoft.com/luis/api/v2.0/apps/${appId}/versions/${versionId}/closedlists`;
+
+// create list entity
+var addListEntity = async () => {
+
+    try {
+
+        // LUIS model definition for list entity
+        var body = {
+            "name": "DevicesList",
+            "sublists": 
+            [
+                {
+                    "canonicalForm": "Thermostat",
+                    "list": [ "ac", "AC","A/C", "A-C","heater","hot","hotter","cold","colder" ]
+                }
+            ]
+        }
+
+        // HTTP request
+        var myHTTPrequest = request({
+            uri: uri,
+            method: 'POST',
+            headers: {
+                'Ocp-Apim-Subscription-Key': programmaticKey
+            },
+            json: true,
+            body: body
+        });
+
+        return await myHTTPrequest;
+
+    } catch (err) {
+        throw err;
+    }
+}
+// MAIN
+addListEntity().then( (response) => {
+    // return GUID of list entity model
+    console.log(response);
+}).catch(err => {
+    console.log(`Error adding list entity:  ${err.message} `);
+});
