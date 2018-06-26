1e9bd8e0a3d817b992c865cf6c177e4ad44f6b82
Dina Berry <v-geberr@microsoft.com>
2018-06-26 15:20:59 -0700

    0626 fix speech to intent (#146)
    * fix speech-to-text
    
    * add links to readme for speech services

--- documentation-samples/tutorial-speech-intent-recognition/csharp/Program.cs
+++ documentation-samples/tutorial-speech-intent-recognition/csharp/Program.cs
@@ -14,14 +14,7 @@ namespace MicrosoftSpeechSDKSamples
         static void Main(string[] args)
         {
 
-            Console.WriteLine("1. Speech recognition with microphone input.");
-            Console.WriteLine("2. Speech recognition in the specified language.");
-            Console.WriteLine("3. Speech recognition with file input.");
-            Console.WriteLine("4. Speech recognition using customized model.");
-            Console.WriteLine("5. Speech continuous recognition using events.");
-            Console.WriteLine("6. Translation with microphone input.");
-            Console.WriteLine("7. Translation with file input.");
-            Console.WriteLine("8. Speech recognition of LUIS intent.");
+            Console.WriteLine("1. Speech recognition of LUIS intent.");
             Console.WriteLine("0. Stop.");
 
             Console.Write("Your choice: ");
@@ -34,27 +27,6 @@ namespace MicrosoftSpeechSDKSamples
                 switch (x.Key)
                 {
                     case ConsoleKey.D1:
-                        SpeechRecognitionSamples.RecognitionWithMicrophoneAsync().Wait();
-                        break;
-                    case ConsoleKey.D2:
-                        SpeechRecognitionSamples.RecognitionWithLanguageAsync().Wait();
-                        break;
-                    case ConsoleKey.D3:
-                        SpeechRecognitionSamples.RecognitionWithFileAsync().Wait();
-                        break;
-                    case ConsoleKey.D4:
-                        SpeechRecognitionSamples.RecognitionUsingCustomizedModelAsync().Wait();
-                        break;
-                    case ConsoleKey.D5:
-                        SpeechRecognitionSamples.ContinuousRecognitionAsync().Wait();
-                        break;
-                    case ConsoleKey.D6:
-                        TranslationSamples.TranslationWithMicrophoneAsync().Wait();
-                        break;
-                    case ConsoleKey.D7:
-                        TranslationSamples.TranslationWithFileAsync().Wait();
-                        break;
-                    case ConsoleKey.D8:
                         Console.WriteLine("LUIS...");
                         LuisSamples.RecognitionWithLUIS().Wait();
                         break;
