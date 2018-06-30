64801824912008bc579a4959f3e5d4c71303dc91
Dina Berry <v-geberr@microsoft.com>
2018-06-30 09:50:48 -0700

    go via cmdline

--- documentation-samples/endpoint-api-samples/go/endpoint.go
+++ documentation-samples/endpoint-api-samples/go/endpoint.go
@@ -1,27 +1,44 @@
 package main
 
-import (
-	"net/http"
-	"fmt"
-	"io/ioutil"
-	"log"
-)
-
 /*
 	build endpoint.go from command line
 	> go build endpoint.go
 
 	run endpoint from command line
-	> endpoint
+	> endpoint -appID xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -endpointKey xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -utterance "turn on the lights" -region westus
+
+
+	output
+
+	appID has value be402ffc-57f4-4e1f-9c1d-f0d9fa520aa4
+	endpointKey has value xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	region has value westus
+	utterance has value turn on the lights
+	{
+	"query": "turn on the lights",
+	"topScoringIntent": {
+		"intent": "Utilities.Stop",
+		"score": 0.457045376
+	},
+	"entities": []
+	}
 
 */
-func main() {
+
+import (
+	"fmt"
+	"flag"
+	"net/http"
+	"net/url"
+	"io/ioutil"
+	"log"
+)
+
+func endpointPrediction(appID string, endpointKey string, region string, utterance string) {
+
+	var endpointUrl = fmt.Sprintf("https://%s.api.cognitive.microsoft.com/luis/v2.0/apps/%s?subscription-key=%s&verbose=false&q=%s", region, appID, endpointKey, url.QueryEscape(utterance))
 	
-
-	// IoT Home Automation app ID = df67dcdb-c37d-46af-88e1-8b97951ca1c2
-	// Add your LUIS endpoint key (not the authoring key)
-
-	response, err := http.Get("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/df67dcdb-c37d-46af-88e1-8b97951ca1c2?subscription-key=<LUIS-endpoint-key>&verbose=false&q=turn%20on%20the%20bedroom%20light")
+	response, err := http.Get(endpointUrl)
 
 	// 401 - check value of 'subscription-key' - do not use authoring key!
 	if err!=nil {
@@ -39,21 +56,20 @@ func main() {
 	fmt.Println(string(response2))
 }
 
-/*
-{
-  "query": "turn on the bedroom light",
-  "topScoringIntent": {
-    "intent": "HomeAutomation.TurnOn",
-    "score": 0.809439957
-  },
-  "entities": [
-    {
-      "entity": "bedroom",
-      "type": "HomeAutomation.Room",
-      "startIndex": 12,
-      "endIndex": 18,
-      "score": 0.8065475
-    }
-  ]
+func main() {
+	
+	var appID = flag.String("appID", "", "LUIS appID")
+	var endpointKey = flag.String("endpointKey", "", "LUIS endpoint key")
+	var region = flag.String("region", "", "LUIS app publish region")
+	var utterance = flag.String("utterance", "", "utterance to predict")
+
+	flag.Parse()
+	
+	fmt.Println("appID has value", *appID)
+	fmt.Println("endpointKey has value", *endpointKey)
+	fmt.Println("region has value", *region)
+	fmt.Println("utterance has value", *utterance)
+
+	endpointPrediction(*appID, *endpointKey, *region, *utterance)
+
 }
-*/
