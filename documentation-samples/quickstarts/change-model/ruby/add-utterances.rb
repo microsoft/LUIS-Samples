3040306e0bd44eb38ccf7d72e2cbe135908f3a41
Dina Berry <diberry@microsoft.com>
2018-08-24 09:27:33 -0700

    authoring docker ruby

--- documentation-samples/quickstarts/change-model/ruby/add-utterances.rb
+++ documentation-samples/quickstarts/change-model/ruby/add-utterances.rb
@@ -7,25 +7,18 @@ require 'uri'
 # **********************************************
 
 # NOTE: Replace this with LUIS application ID.
-appID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+appID = "YOUR-APP-ID"
 
 # NOTE: Replace this example LUIS application version number with the version number of your LUIS application.
 appVersion = "0.1"
 
 # NOTE: Replace this with LUIS AUTHORING KEY.
-$key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+$key = "YOUR-AUTHORING-KEY"
 
 host = "https://westus.api.cognitive.microsoft.com"
 path = "/luis/api/v2.0/apps/" + appID + "/versions/" + appVersion + "/"
 $uri = host + path;
-
-usage = "Usage:
-add-utterances <input file>
-add-utterances -train <input file>
-add-utterances -status
-
-The contents of <input file> must be in the format described at: https://aka.ms/add-utterance-json-format
-"
+$file = "./utterances.json"
 
 def SendGet(uri)
 	uri = URI(uri)
@@ -60,8 +53,8 @@ def AddUtterances(input_file)
 	puts JSON.pretty_generate(JSON(result))
 end
 
-def Train(input_file)
-	content = File.read(input_file)
+def Train()
+	content = null
 	puts "Sent training request."
 	result = SendPost($uri+"train",content)
     puts JSON.pretty_generate(JSON(result))
@@ -74,20 +67,6 @@ def Status()
     puts JSON.pretty_generate(JSON(result))
 end
 
-args = ARGV
-
-if (args.length < 1) then
-	puts(usage)
-else
-	if (0 == args[0].casecmp("-train"))
-		if (args.length > 1) then
-			Train(args[1])
-		else
-			puts(usage)
-		end
-	elsif (0 == args[0].casecmp("-status")) then
-		Status()
-	else
-		AddUtterances(args[0])
-	end
-end
+AddUtterances($file)
+Train()
+Status()
