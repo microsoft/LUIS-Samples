69d9ae219773d032f41092fdb71b4eebc6a19ffd
Dina Berry <41597107+diberry@users.noreply.github.com>
2018-10-06 12:25:28 -0700

    github.com/MicrosoftDocs/azure-docs/issues/15854

--- documentation-samples/tutorial-speech-intent-recognition/csharp/Program.cs
+++ documentation-samples/tutorial-speech-intent-recognition/csharp/Program.cs
@@ -6,6 +6,8 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Intent;
 
 namespace MicrosoftSpeechSDKSamples
 {
@@ -13,32 +15,76 @@ namespace MicrosoftSpeechSDKSamples
     {
         static void Main(string[] args)
         {
+            RecognizeAsync().Wait();
+            Console.WriteLine("Please press Enter to continue.");
+            Console.ReadLine();
+        }
+        static async Task RecognizeAsync()
+        {
 
-            Console.WriteLine("1. Speech recognition of LUIS intent.");
-            Console.WriteLine("0. Stop.");
+            string LUIS_assigned_endpoint_key = "<LUIS-assigned-resource-key>";
+            string LUIS_endpoint_key_region = "westus";
+            string LUIS_app_ID = "<LUIS-app-ID>";
 
-            Console.Write("Your choice: ");
+            // Creates an instance of a speech config with specified subscription key
+            // and service region. Note that in contrast to other services supported by
+            // the Cognitive Services Speech SDK, the Language Understanding service
+            // requires a specific subscription key from https://www.luis.ai/.
+            // The Language Understanding service calls the required key 'endpoint key'.
+            // Once you've obtained it, replace with below with your own Language Understanding subscription key
+            // and service region (e.g., "westus").
+            // The default language is "en-us".
+            var config = SpeechConfig.FromSubscription(LUIS_assigned_endpoint_key, LUIS_endpoint_key_region);
 
-            ConsoleKeyInfo x;
-            do
+            // Creates an intent recognizer using microphone as audio input.
+            using (var recognizer = new IntentRecognizer(config))
             {
-                x = Console.ReadKey();
-                Console.WriteLine("");
-                switch (x.Key)
+                // Creates a Language Understanding model using the app id, and adds specific intents from your model
+                var model = LanguageUnderstandingModel.FromAppId(LUIS_app_ID);
+
+                // Add intents from HumanResources.json to your intent recognizer
+                recognizer.AddIntent(model, "None", "None");
+                recognizer.AddIntent(model, "FindForm", "FindForm");
+                recognizer.AddIntent(model, "GetEmployeeBenefits", "GetEmployeeBenefits");
+                recognizer.AddIntent(model, "GetEmployeeOrgChart", "GetEmployeeOrgChart");
+                recognizer.AddIntent(model, "MoveAssetsOrPeople", "MoveAssetsOrPeople");
+
+                // Starts recognizing.
+                Console.WriteLine("Say something...");
+
+                // Performs recognition. RecognizeOnceAsync() returns when the first utterance has been recognized,
+                // so it is suitable only for single shot recognition like command or query. For long-running
+                // recognition, use StartContinuousRecognitionAsync() instead.
+                var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+
+                // Checks result.
+                if (result.Reason == ResultReason.RecognizedIntent)
                 {
-                    case ConsoleKey.D1:
-                        Console.WriteLine("LUIS...");
-                        LuisSamples.RecognitionWithLUIS().Wait();
-                        break;
-                    case ConsoleKey.D0:
-                        Console.WriteLine("Exiting...");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input.");
-                        break;
+                    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+                    Console.WriteLine($"    Intent Id: {result.IntentId}.");
+                    Console.WriteLine($"    Language Understanding JSON: {result.Properties.GetProperty(PropertyId.LanguageUnderstandingServiceResponse_JsonResult)}.");
                 }
-                Console.WriteLine("\nRecognition done. Your Choice: ");
-            } while (x.Key != ConsoleKey.D0);
+                else if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+                    Console.WriteLine($"    Intent not recognized.");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+            }
         }
     }
 }
